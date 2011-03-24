ed61fe1f6cb861a12854d088086917dfd90ddd9d
Oliver Caldwell <oliver@flowdev.co.uk>
2011-03-24 13:21:26 +0000

    Incremented the version in the header comment

--- mappa.js
+++ mappa.js
@@ -1,5 +1,5 @@
 /**
- * @preserve Mappa v1.0.0 (github.com/Wolfy87/Mappa)
+ * @preserve Mappa v1.1.0 (github.com/Wolfy87/Mappa)
  * Copyright 2011, Oliver Caldwell (flowdev.co.uk)
  */
 (function() {
